cdf9cc0678ea607b828b0d5dbbcb4ad6f37b2b9e
Vitalii Terets <vitaliipool@gmail.com>
2023-06-11 01:19:35 +0200

    Summary

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,24 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+window.addEventListener("DOMContentLoaded", () => {
+	const refs = {
+		openModalBtn: document.querySelector("[data-modal-open]"),
+		closeModalBtn: document.querySelector("[data-modal-close]"),
+		modal: document.querySelector("[data-modal]"),
+	};
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+	refs.openModalBtn.addEventListener("click", toggleModal);
+	refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
+	function toggleModal() {
+		refs.modal.classList.toggle("is-hidden");
+	}
+
+	const modalWindow = document.querySelector(".backdrop");
+
+	function setDuration() {
+		setTimeout(function () {
+			modalWindow.style.transitionDuration = "250ms";
+		}, 500);
+	}
+
+	setDuration();
+});
